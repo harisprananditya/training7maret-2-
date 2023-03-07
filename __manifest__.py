5ce6aac3d54817fb14cb416255a6246a5a000785
harisprananditya <127082591+harisprananditya@users.noreply.github.com>
2023-03-07 14:13:02 +0700

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+
+{
+	'name': "Training"
+	'summary': """Odoo Training"""
+	'description': """ 
+		Training module for managing trainings:
+			- traininng courses
+	""",
+
+
+	'author': "My Company",
+	'website': "http://www.yourcompany.com"
+	'category': "Test",
+	'version': "0.1",
+
+	'depends': [
+		'base',
+	],
+
+	'data' : [
+	],
+	'demo': [
+	],
+
+}
